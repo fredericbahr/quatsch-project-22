23d8daad0c4648f71e49a84e6b9f9ed137937f8a
sdomenec <sergi.domenech_guzy@stud.htwk-leipzig.de>
2023-02-04 18:51:41 +0100

    chore: add more questions & improved code quality

--- packages/qanary-ner-training-data/generateTrainingData.js
+++ packages/qanary-ner-training-data/generateTrainingData.js
@@ -56,95 +56,22 @@ const measurands = [
   "pm25k",
 ];
 
-const calculations = ["Minimum", "Maximum", "Durchschnitt", "Grenzwert"];
+const calculations = [
+  "Minimum",
+  "Minima",
+  "minimal",
+  "Maximum",
+  "Maxima",
+  "maximal",
+  "Durchschnitt",
+  "durchschnittlich",
+  "Grenzwert",
+  "kritisch",
+];
 
 const representations = ["Text", "Tabelle", "Liste", "Graph"];
 
-const trainingData = [];
-const nluExamples = [];
-
-const generateString = (string = null, prefix = "", suffix = "") => {
-  return string ? `${prefix}${string}${suffix}` : undefined;
-};
-
-const addToLists = (
-  question,
-  { station, measurand, representation, calculation }
-) => {
-  measurandString = generateString(
-    measurand,
-    question.measurandPrefix,
-    question.measurandSuffix
-  );
-  representationString = generateString(
-    representation,
-    question.representationPrefix,
-    question.representationSuffix
-  );
-  calculationString = generateString(
-    calculation,
-    question.calculationPrefix,
-    question.calculationSuffix
-  );
-
-  trainingData.push({
-    text: question.text({
-      station,
-      measurand: measurandString,
-      representation: representationString,
-      calculation: calculationString,
-    }),
-    language: "de",
-    entities: {
-      station,
-      measurand: measurandString,
-      representation: representationString,
-      calculation: calculationString,
-    },
-  });
-  nluExamples.push(
-    question.text({
-      station: `[${station}](station)`,
-      measurand: `[${measurandString}](measurand)`,
-      representation: `[${representationString}](representation)`,
-      calculation: `[${calculationString}](calculation)`,
-    })
-  );
-};
-
-const writeJsonFile = (data) => {
-  const trainJson = {
-    trainingdata: data,
-  };
-
-  fs.writeFile("./train.json", JSON.stringify(trainJson, null, 2), (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("qanary ner component training data written to 'train.json'");
-    }
-  });
-};
-
-const writeYmlFile = (data) => {
-  const trainYml = `version: "3.1"
-
-nlu:
-  ## Creation of context
-  - intent: context_air_measurand
-    examples: |
-    - ${data.join("\n    - ")}`;
-
-  fs.writeFile("./air_measurand.yml", trainYml, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("rasa nlu examples written to 'air_measurand.yml'");
-    }
-  });
-};
-
-const questions1 = [
+const questionsStationMeasurand = [
   {
     text: ({ measurand, station }) => `Wie ist der ${measurand} in ${station}?`,
     measurandAllowList: [
@@ -177,7 +104,7 @@ const questions1 = [
   },
 ];
 
-const questions2 = [
+const questionsStationMeasurandRepresentation = [
   {
     text: ({ measurand, station, representation }) =>
       `Zeig mir eine ${representation} der ${measurand} Werte der letzten Woche aus ${station}.`,
@@ -236,9 +163,25 @@ const questions2 = [
     representationSuffix: "en",
     measurandSuffix: "werte",
   },
+  {
+    text: ({ measurand, station, representation }) =>
+      `Welcher ${measurand} Wert wird gerade in ${station} gemessen? Repräsentiert als ${representation}.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Text"],
+  },
 ];
 
-const questions3 = [
+const questionsStationMeasurandCalculation = [
   {
     text: ({ measurand, station, calculation }) =>
       `Wie hoch war der ${measurand} in ${station} gestern im ${calculation}?`,
@@ -257,7 +200,6 @@ const questions3 = [
       "pm10",
       "pm25k",
     ],
-    representationAllowList: ["Text", "Tabelle", "Liste", "Graph"],
     calculationAllowList: ["Minimum", "Maximum", "Durchschnitt"],
   },
   {
@@ -274,12 +216,110 @@ const questions3 = [
       "pm10",
       "pm25k",
     ],
-    representationAllowList: ["Text", "Tabelle", "Liste", "Graph"],
     calculationAllowList: ["Minimum", "Maximum", "Durchschnitt"],
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Wie hoch war gestern der ${calculation} ${measurand} in ${station}?`,
+    measurandAllowList: ["Stickstoffdioxid", "Ozon", "Feinstaub"],
+    calculationAllowList: ["minimal", "maximal", "durchschnittlich"],
+    measurandSuffix: "wert",
+    calculationSuffix: "e",
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Wie hoch war gestern der ${calculation} ${measurand} in ${station}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    calculationAllowList: ["minimal", "maximal", "durchschnittlich"],
+    calculationSuffix: "e",
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Ich hätte gerne das ${calculation} vom ${measurand} in ${station} des letzten Jahres.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    calculationAllowList: ["Minimum", "Maximum"],
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Was ist der aktuelle ${measurand} Wert in ${station} im Vergleich zum ${calculation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    calculationAllowList: ["Grenzwert"],
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Was ist der aktuelle ${measurand} in ${station} im Vergleich zum ${calculation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+    ],
+    calculationAllowList: ["Grenzwert"],
+    measurandSuffix: "wert",
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Was ist der aktuelle ${measurand} in ${station} im Vergleich zum ${calculation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    calculationAllowList: ["Grenzwert"],
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Ist der aktuelle Wert des ${measurand} in ${station} ${calculation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    calculationAllowList: ["kritisch"],
+  },
+  {
+    text: ({ measurand, station, calculation }) =>
+      `Ist der aktuelle Wert des ${measurand} in ${station} ${calculation}?`,
+    measurandAllowList: ["Stickstoffdioxid", "Ozon", "Feinstaub"],
+    calculationAllowList: ["kritisch"],
+    measurandSuffix: "s",
   },
 ];
 
-const questions4 = [
+const questionsStationMeasurandRepresentationCalculation = [
   {
     text: ({ measurand, station, representation, calculation }) =>
       `Wie war der ${measurand} in ${station} am 23.01.2023, verglichen mit den ${calculation} der letzten 10 Jahre repräsentiert als ${representation}?`,
@@ -312,76 +352,339 @@ const questions4 = [
     calculationAllowList: ["Minimum", "Maximum", "Durchschnitt"],
     calculationSuffix: "swerten",
   },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Erstelle mir eine ${representation} für den ${calculation} der ${measurand} Werte von ${station} für die letzten 5 Jahre.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Tabelle", "Liste"],
+    calculationAllowList: ["Durchschnitt"],
+  },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Erstelle mir einen ${representation} für den ${calculation} der ${measurand} Werte von ${station} für die letzten 4 Wochen.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Graph"],
+    calculationAllowList: ["Durchschnitt"],
+    representationSuffix: "en",
+  },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Was waren die ${calculation} der ${measurand} Messungen der letzten Woche in ${station}, repräsentiert als ${representation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Tabelle", "Liste", "Graph"],
+    calculationAllowList: ["Minima", "Maxima"],
+  },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Was waren die ${calculation} der ${measurand} Messungen der letzten Woche in ${station}, repräsentiert als ${representation}?`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Tabelle", "Liste", "Graph"],
+    calculationAllowList: ["Durchschnitt"],
+    calculationSuffix: "swerte",
+  },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Gib mir den ${calculation} ${measurand} in ${station} der letzten 4 Tage als ${representation}.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+    ],
+    representationAllowList: ["Text", "Tabelle", "Liste", "Graph"],
+    calculationAllowList: ["minimal", "maximal", "durchschnittlich"],
+    measurandSuffix: "wert",
+    calculationSuffix: "en",
+  },
+  {
+    text: ({ measurand, station, representation, calculation }) =>
+      `Gib mir den ${calculation} ${measurand} Wert in ${station} der letzten 4 Tage als ${representation}.`,
+    measurandAllowList: [
+      "Luftqualitätsindex",
+      "Stickstoffdioxid",
+      "Ozon",
+      "Feinstaub",
+      "luqx",
+      "no2",
+      "o3",
+      "pm10",
+      "pm25k",
+    ],
+    representationAllowList: ["Text", "Tabelle", "Liste", "Graph"],
+    calculationAllowList: ["minimal", "maximal", "durchschnittlich"],
+    calculationSuffix: "en",
+  },
 ];
 
-questions1.forEach((question) => {
-  stations.forEach((station) => {
-    measurands
-      .filter((measurand) => question.measurandAllowList.includes(measurand))
-      .forEach((measurand) => {
-        addToLists(question, { station, measurand });
-      });
-  });
-});
+const nerData = [];
+const nluData = [];
 
-questions2.forEach((question) => {
-  stations.forEach((station) => {
-    measurands
-      .filter((measurand) => question.measurandAllowList.includes(measurand))
-      .forEach((measurand) => {
-        representations
-          .filter((representation) =>
-            question.representationAllowList.includes(representation)
-          )
-          .forEach((representation) => {
-            addToLists(question, { station, measurand, representation });
-          });
-      });
-  });
-});
+/**
+ * Generates a string out of a given input base string and a prefix and suffix.
+ * @param {*} baseString base string to be extended
+ * @param {*} prefix
+ * @param {*} suffix
+ * @returns a pre- and suffixed string or undefined
+ */
+const generateString = (baseString = null, prefix = "", suffix = "") => {
+  return baseString ? `${prefix}${baseString}${suffix}` : undefined;
+};
 
-questions3.forEach((question) => {
-  stations.forEach((station) => {
-    measurands
-      .filter((measurand) => question.measurandAllowList.includes(measurand))
-      .forEach((measurand) => {
-        calculations
-          .filter((calculation) =>
-            question.calculationAllowList.includes(calculation)
-          )
-          .forEach((calculation) => {
-            addToLists(question, { station, measurand, calculation });
-          });
-      });
-  });
-});
+/**
+ * Generates training data for ner and nlu from question and provided data
+ * @param {*} question question object
+ * @param {*} data data object containing station, measurand, representation, calculation
+ * @returns trainingDataNer and trainingDataNlu
+ */
+const generateTrainingData = (
+  question,
+  { station, measurand, representation, calculation }
+) => {
+  const measurandString = generateString(
+    measurand,
+    question.measurandPrefix,
+    question.measurandSuffix
+  );
+  const representationString = generateString(
+    representation,
+    question.representationPrefix,
+    question.representationSuffix
+  );
+  const calculationString = generateString(
+    calculation,
+    question.calculationPrefix,
+    question.calculationSuffix
+  );
 
-questions4.forEach((question) => {
-  stations.forEach((station) => {
-    measurands
-      .filter((measurand) => question.measurandAllowList.includes(measurand))
-      .forEach((measurand) => {
-        representations
-          .filter((representation) =>
-            question.representationAllowList.includes(representation)
-          )
-          .forEach((representation) => {
-            calculations
-              .filter((calculation) =>
-                question.calculationAllowList.includes(calculation)
-              )
-              .forEach((calculation) => {
-                addToLists(question, {
-                  station,
-                  measurand,
-                  representation,
-                  calculation,
+  const trainingDataNer = {
+    text: question.text({
+      station,
+      measurand: measurandString,
+      representation: representationString,
+      calculation: calculationString,
+    }),
+    language: "de",
+    entities: {
+      station,
+      measurand: measurandString,
+      representation: representationString,
+      calculation: calculationString,
+    },
+  };
+
+  const trainingDataNlu = question.text({
+    station: `[${station}](station)`,
+    measurand: `[${measurandString}](measurand)`,
+    representation: `[${representationString}](representation)`,
+    calculation: `[${calculationString}](calculation)`,
+  });
+
+  return { trainingDataNer, trainingDataNlu };
+};
+
+/**
+ * Adds provided training data objects to data lists
+ * @param {*} trainingDataNer training data object for qanary ner conponent
+ * @param {*} trainingDataNlu training data string for rasa nlu
+ */
+const addToLists = (trainingDataNer, trainingDataNlu) => {
+  nerData.push(trainingDataNer);
+  nluData.push(trainingDataNlu);
+};
+
+/**
+ * Writes provided data with necessary structure for qanary ner component into ner-train.json file
+ * @param {*} data training data array
+ */
+const writeJsonFile = (data) => {
+  const trainJson = {
+    trainingdata: data,
+  };
+
+  fs.writeFile(
+    "./ner-train.json",
+    JSON.stringify(trainJson, null, 2),
+    (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(
+          "qanary ner component training data written to 'ner-train.json'"
+        );
+      }
+    }
+  );
+};
+
+/**
+ * Writes provided data with necessary structure for rasa nlu into nlu-train.yml file
+ * @param {*} data training data array
+ */
+const writeYmlFile = (data) => {
+  const trainYml = `version: "3.1"
+
+nlu:
+  ## Creation of context
+  - intent: context_air_measurand
+    examples: |
+    - ${data.join("\n    - ")}`;
+
+  fs.writeFile("./nlu-train.yml", trainYml, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("rasa nlu examples written to 'nlu-train.yml'");
+    }
+  });
+};
+
+/**
+ * generates training data for questions with station and measurand slots and adds data to lists
+ */
+const generateStationMeasurandData = () => {
+  questionsStationMeasurand.forEach((question) => {
+    stations.forEach((station) => {
+      measurands
+        .filter((measurand) => question.measurandAllowList.includes(measurand))
+        .forEach((measurand) => {
+          const { trainingDataNer, trainingDataNlu } = generateTrainingData(
+            question,
+            { station, measurand }
+          );
+          addToLists(trainingDataNer, trainingDataNlu);
+        });
+    });
+  });
+};
+
+/**
+ * generates training data for questions with station, measurand and representation slots and adds data to lists
+ */
+const generateStationMeasurandRepresentationData = () => {
+  questionsStationMeasurandRepresentation.forEach((question) => {
+    stations.forEach((station) => {
+      measurands
+        .filter((measurand) => question.measurandAllowList.includes(measurand))
+        .forEach((measurand) => {
+          representations
+            .filter((representation) =>
+              question.representationAllowList.includes(representation)
+            )
+            .forEach((representation) => {
+              const { trainingDataNer, trainingDataNlu } = generateTrainingData(
+                question,
+                { station, measurand, representation }
+              );
+              addToLists(trainingDataNer, trainingDataNlu);
+            });
+        });
+    });
+  });
+};
+
+/**
+ * generates training data for questions with station, measurand and calculation slots and adds data to lists
+ */
+const generateStationMeasurandCalculationData = () => {
+  questionsStationMeasurandCalculation.forEach((question) => {
+    stations.forEach((station) => {
+      measurands
+        .filter((measurand) => question.measurandAllowList.includes(measurand))
+        .forEach((measurand) => {
+          calculations
+            .filter((calculation) =>
+              question.calculationAllowList.includes(calculation)
+            )
+            .forEach((calculation) => {
+              const { trainingDataNer, trainingDataNlu } = generateTrainingData(
+                question,
+                { station, measurand, calculation }
+              );
+              addToLists(trainingDataNer, trainingDataNlu);
+            });
+        });
+    });
+  });
+};
+
+/**
+ * generates training data for questions with station, measurand, representation and calculation slots and adds data to lists
+ */
+const generateStationMeasurandRepresentationCalculationData = () => {
+  questionsStationMeasurandRepresentationCalculation.forEach((question) => {
+    stations.forEach((station) => {
+      measurands
+        .filter((measurand) => question.measurandAllowList.includes(measurand))
+        .forEach((measurand) => {
+          representations
+            .filter((representation) =>
+              question.representationAllowList.includes(representation)
+            )
+            .forEach((representation) => {
+              calculations
+                .filter((calculation) =>
+                  question.calculationAllowList.includes(calculation)
+                )
+                .forEach((calculation) => {
+                  const { trainingDataNer, trainingDataNlu } =
+                    generateTrainingData(question, {
+                      station,
+                      measurand,
+                      representation,
+                      calculation,
+                    });
+                  addToLists(trainingDataNer, trainingDataNlu);
                 });
-              });
-          });
-      });
+            });
+        });
+    });
   });
-});
+};
+
+generateStationMeasurandData();
+generateStationMeasurandRepresentationData();
+generateStationMeasurandCalculationData();
+generateStationMeasurandRepresentationCalculationData();
 
 writeJsonFile(trainingData);
 writeYmlFile(nluExamples);
