01c3b351a7906bd74070df8b55c082beb99cbc9b
Sergi Domenech Guzy <sergi.domenech_guzy@stud.htwk-leipzig.de>
2023-03-11 12:35:24 +0100

    docs: add result example

--- apps/qanary-component-ner-automl/src/generateTrainingData/generateNerDomainBaseData.ts
+++ apps/qanary-component-ner-automl/src/generateTrainingData/generateNerDomainBaseData.ts
@@ -6,6 +6,18 @@ import generateLubwData from "../utils/generateLubwData";
 const { calculations, measurands, representations, stations } = generateLubwData();
 /**
  * Generates ner training data object from domain entry name and provided entityKey.
+ *
+ * @example Base domain entry ner training data object for a station
+ * ```ts
+ *  const example: NerTrainingData = {
+ *    text: "Kehl",
+ *    language: "de",
+ *    entities: {
+ *      station: "Kehl",
+ *    }
+ *  }
+ * ```
+ *
  * @param domainEntry domain entry name used as text and entity
  * @param entityKey
  * @returns ner training data object for base domain entry
