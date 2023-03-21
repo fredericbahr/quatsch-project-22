c81732f64ee936a63eb130a547ac6189a9db1353
Frederic Bahr <fredericbahr1@gmail.com>
2023-03-21 21:28:09 +0100

    fix: fix tests

--- apps/action-server/src/resources/fallback/default/__tests__/default-quanary.spec.ts
+++ apps/action-server/src/resources/fallback/default/__tests__/default-quanary.spec.ts
@@ -1,25 +1,29 @@
 import { RasaRequest, RasaResponse } from "shared";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { abstractRequestHandler } from "../../../measurand/abstract/abstract.controller";
 import { fallbackRequestHandler } from "../fallback.controller";
+
+
+jest.mock("../../../measurand/abstract/abstract.controller", () => ({
+  abstractRequestHandler: jest.fn(),
+}));
 
 describe("#Action Default Qanary", () => {
   const req: RasaRequest = { body: { tracker: { latest_message: { text: "test" } } } } as RasaRequest;
   const res: RasaResponse = { status: jest.fn(), end: jest.fn(), json: jest.fn() } as unknown as RasaResponse;
 
+  const mockAbstractRequestHandler: jest.Mock = jest.fn();
+
+  beforeEach(() => {
+    (abstractRequestHandler as jest.Mock) = mockAbstractRequestHandler;
+  });
+
   afterEach(() => jest.clearAllMocks());
 
-  it("should return a text response", () => {
-    fallbackRequestHandler(req, res);
+  it("should call abstract request handler", async () => {
+    await fallbackRequestHandler(req, res);
 
-    expect(res.end).toHaveBeenCalled();
-    expect(res.json).toHaveBeenCalledWith(
-      expect.objectContaining({
-        responses: expect.arrayContaining([
-          expect.objectContaining({
-            text: "Anfrage wurde durch Qanary-Default-Pipeline beantwortet.",
-          }),
-        ]),
-      }),
-    );
+    expect(mockAbstractRequestHandler).toBeCalled();
   });
 });
